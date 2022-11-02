2a9b6759853d576caad075ac96b77a45d4bd89e4
Deivison Savio Casimiro <deivison.savio@gmail.com>
2022-11-02 15:05:46 -0300

    Adicionando novo projeto utilizando metodos

---
+++ sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,26 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+       
+        SmartTv smartTv = new SmartTv();
+
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumetarVolume();
+
+        System.out.println("Canal Atual: " + smartTv.canal);
+
+        smartTv.mudarCanal(13);
+
+
+        //System.out.println("TV Ligada? " + smartTv.ligada);
+        System.out.println("Canal atual: " + smartTv.canal);
+        System.out.println("Volume Atual: " + smartTv.volume);
+
+        smartTv.ligar();
+        System.out.println("\nTV Ligada? " + smartTv.ligada);
+
+        smartTv.desligar();
+        System.out.println("\nTV Ligada? " + smartTv.ligada);
+    }
+}
